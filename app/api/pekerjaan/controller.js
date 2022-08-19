fa2fdf0fa40caee98bb84bc76f120df92c8acc7a
FarBb <loveinsemarang@gmail.com>
2022-08-20 02:08:02 +0700

    update

--- app/api/pekerjaan/controller.js
+++ app/api/pekerjaan/controller.js
@@ -1,9 +1,12 @@
 const Pekerjaan = require("../../pekerjaan/model");
+const path = require("path");
+const fs = require("fs");
+const config = require("../../../config");
 
 module.exports = {
   landingPage: async (req, res) => {
     try {
-      const apiData = await Pekerjaan.find().select("_id pekerjaan thumbnail");
+      const apiData = await Pekerjaan.find().select("_id pekerjaan thumbnial");
       res.status(200).json({ total: apiData.length, data: apiData });
     } catch (err) {
       res.status(500).json({ message: err.message || `Internal server error` });
@@ -21,12 +24,91 @@ module.exports = {
   actionCreate: async (req, res) => {
     try {
       const { pekerjaan, deskripsiPekerjaan } = req.body;
-      const apiData = new Pekerjaan({
-        pekerjaan,
-        deskripsiPekerjaan,
-      });
-      await apiData.save();
-      res.status(200).json({ message: "Success", data: apiData });
+      if (req.file) {
+        let tmp_path = req.file.path;
+        let originaExt = req.file.originalname.split(".")[req.file.originalname.split(".").length - 1];
+        let filename = req.file.filename + "." + originaExt;
+        let target_path = path.resolve(config.rootPath, `public/uploads/banner-pekerjaan/${filename}`);
+
+        const src = fs.createReadStream(tmp_path);
+        const dest = fs.createWriteStream(target_path);
+
+        src.pipe(dest);
+        src.on("end", async () => {
+          try {
+            const apiData = new Pekerjaan({
+              pekerjaan,
+              deskripsiPekerjaan,
+              thumbnial: filename,
+            });
+            await apiData.save();
+            res.status(200).json({ msg: "success", data: apiData });
+          } catch (err) {
+            res.status(500).json({ message: err.message || `Internal server error` });
+          }
+        });
+      } else {
+        const apiData = new Pekerjaan({
+          pekerjaan,
+          deskripsiPekerjaan,
+        });
+        await apiData.save();
+        res.status(200).json({ msg: "success", data: apiData });
+      }
+    } catch (err) {
+      res.status(500).json({ message: err.message || `Internal server error` });
+    }
+  },
+  actionEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { pekerjaan, deskripsiPekerjaan } = req.body;
+
+      if (req.file) {
+        let tmp_path = req.file.path;
+        let originaExt = req.file.originalname.split(".")[req.file.originalname.split(".").length - 1];
+        let filename = req.file.filename + "." + originaExt;
+        let target_path = path.resolve(config.rootPath, `public/uploads/banner-pekerjaan/${filename}`);
+
+        const src = fs.createReadStream(tmp_path);
+        const dest = fs.createWriteStream(target_path);
+
+        src.pipe(dest);
+        src.on("end", async () => {
+          try {
+            const apiData = await Pekerjaan.findOne({ _id: id });
+
+            let currentImage = `${config.rootPath}/public/uploads/banner-pekerjaan/${apiData.thumbnial}`;
+            if (fs.existsSync(currentImage)) {
+              fs.unlinkSync(currentImage);
+            }
+            await Pekerjaan.findOneAndUpdate(
+              {
+                _id: id,
+              },
+              {
+                pekerjaan,
+                deskripsiPekerjaan,
+                thumbnial: filename,
+              }
+            );
+            res.status(200).json({ msg: "success", data: apiData });
+          } catch (err) {
+            res.status(500).json({ message: err.message || `Internal server error` });
+          }
+        });
+      } else {
+        const apiData = await Pekerjaan.findOneAndUpdate(
+          {
+            _id: id,
+          },
+          {
+            pekerjaan,
+            deskripsiPekerjaan,
+          }
+        );
+        res.status(200).json({ msg: "success", data: apiData });
+      }
     } catch (err) {
       res.status(500).json({ message: err.message || `Internal server error` });
     }
@@ -34,9 +116,13 @@ module.exports = {
   actionDelete: async (req, res) => {
     try {
       const { id } = req.params;
-      await Pekerjaan.findOneAndRemove({
+      const apiData = await Pekerjaan.findOneAndRemove({
         _id: id,
       });
+      let currentFile = `${config.rootPath}/public/uploads/banner-pekerjaan/${apiData.file}`;
+      if (fs.existsSync(currentFile)) {
+        fs.unlinkSync(currentFile);
+      }
       res.status(200).json({ message: "Data Berhasil Dihapus" });
     } catch (err) {
       res.status(500).json({ message: err.message || `Internal server error` });
--- app/api/pekerjaan/router.js
+++ app/api/pekerjaan/router.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var router = express.Router();
-const { landingPage, detailPage, actionCreate, actionDelete } = require("./controller");
+const multer = require("multer");
+const os = require("os");
+const { landingPage, detailPage, actionCreate, actionDelete, actionEdit } = require("./controller");
 
 router.get("/landingpage", landingPage);
 router.get("/detail/:id", detailPage);
-router.post("/add", actionCreate);
+router.post("/add", multer({ dest: os.tmpdir() }).single("thumbnial"), actionCreate);
+router.put("/update/:id", multer({ dest: os.tmpdir() }).single("thumbnial"), actionEdit);
 router.delete("/delete/:id", actionDelete);
 
 module.exports = router;
